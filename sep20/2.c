71673c900c8d6b0a68083837d21222a8625943d6
Arameh <aramehhaghverdian@gmail.com>
2026-09-20 11:59:34 +0400

    fullshell0

--- sep20/2.c
+++ sep20/2.c
@@ -24,6 +24,17 @@ int main(int argc, const char* argv)
     pid_t mypid = getpid();
     printf("parent is running pid=%d\n", mypid);
 
+    char * path = argv[1];
+    char * argvv[argc-1];
+
+    for(int i = 0 ; i<argc ; i++)
+        for (int j = 1 ; j<argc; j++)
+        {
+            argvv[i]=argv[j];
+        }
+
+
+
     pid_t pid = ForkWrap();
     if(pid == 0)
     {           //this scope is the childs code since child gets pid==0
@@ -31,7 +42,7 @@ int main(int argc, const char* argv)
         pid_t mypid = getpid();
         printf("childe is running pid=%d ppid=%d\n", mypid, ppid);
 
-        execlp("ls", "ls", "-l", "-a", NULL);
+        execvp(path, argvv);
 
         perror("failed exec");
         exit(1);
