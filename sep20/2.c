5f08a9afbbc9446c4d9d42d3410615a41a50584e
Arameh <aramehhaghverdian@gmail.com>
2026-09-20 12:05:40 +0400

    works

--- sep20/2.c
+++ sep20/2.c
@@ -7,60 +7,55 @@
 
 pid_t ForkWrap()
 {
-    pid_t pid = fork();
-    if(pid<0)
-    {
-        perror("fork did not happen\n");
-        exit(1);
-    }
-    else
-    {
-        return pid;
-    }
-}
-
-int main(int argc, const char* argv)
+pid_t pid = fork();
+if(pid<0)
 {
-    pid_t mypid = getpid();
-    printf("parent is running pid=%d\n", mypid);
-
-    char * path = argv[1];
-    char * argvv[argc-1];
-
-    for(int i = 0 ; i<argc ; i++)
-        for (int j = 1 ; j<argc; j++)
-        {
-            argvv[i]=argv[j];
-        }
-
-
-
-    pid_t pid = ForkWrap();
-    if(pid == 0)
-    {           //this scope is the childs code since child gets pid==0
-        pid_t ppid = getppid();
-        pid_t mypid = getpid();
-        printf("childe is running pid=%d ppid=%d\n", mypid, ppid);
-
-        execvp(path, argvv);
-
-        perror("failed exec");
-        exit(1);
-    }
-    else if (pid > 0)
-    {
-        int status;
-        wait(&status);
-
-        if(WIFEXITED(status))
-        {
-            printf("child was exited %d \n", status);
-        }
-
-        
-    }
-
-    printf("parent done\n");
-    return 0;
+perror("fork did not happen");
+exit(1);
+}
+else
+{
+return pid;
+}
 }
 
+int main(int argc, char* argv[])
+{
+
+if(argc < 2)
+{
+exit(1);
+}
+
+pid_t mypid = getpid();
+printf("parent is running pid=%d\n", mypid);
+
+char * path = argv[1];
+
+pid_t pid = ForkWrap();
+if(pid == 0)
+{           //this scope is the childs code since child gets pid==0
+pid_t ppid = getppid();
+pid_t mypid = getpid();
+printf("childe is running pid=%d ppid=%d\n", mypid, ppid);
+
+execvp(path, &argv[1]);  
+
+perror("failed exec");
+exit(1);
+}
+else if (pid > 0)
+{
+int status;
+wait(&status);
+
+if(WIFEXITED(status))
+{
+printf("child was exited %d \n", WEXITSTATUS(status));
+}
+
+}
+
+printf("parent done\n");
+return 0;
+}
